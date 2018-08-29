73766cb42b88d2dcec1f6c98418475af3c82413e
T <shrmrf@users.noreply.github.com>
2018-08-29 21:13:02 +0200

    almost final solution

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,26 @@ fn main() {
     let str_a = "conserve";
     let str_b = "converse";
 
+    assert!(str_a.len() == str_b.len());
     let zippified_a: Vec<_> = str_a.chars().collect();
     let zippified_b: Vec<_> = str_b.chars().collect();
 
-    let zippified: Vec<_> = zippified_a.iter().
-                            zip(
+    let zippified: Vec<_> = zippified_a.iter()
+                            .zip(
                                 zippified_b.iter()
-                                ).
-                            filter(
+                                )
+                            .filter(
                                 |(a, b)| a != b
-                            ).collect();
+                                )
+                            .collect();
 
-    
-    // let zippified = str_a.chars().zip(str_b.chars().iter());
-    // let mappified = zippified.take_while(
-    //     |some_char_tuple| some_char_tuple.0 != some_char_tuple.1
-    // ).collect();
-//    str_a.chars().collect()
+    assert_eq!(zippified.len(), 2);
+
+    assert!(
+        (zippified[0].0 == zippified[1].1) &&
+        (zippified[0].1 == zippified[1].0)
+    );
+
     println!("{:?}, {:?}", zippified_a, zippified_b);
     println!("{:?}", zippified);
 }
